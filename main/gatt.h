66398729393e538b6ceef63cd22654290d5b7bbb
U-DOMAIN\avpavlovic <avpavlovic@rtrkw823.domain.local>
2018-07-05 13:31:30 +0200

    Refactor to prepare for A2DP shit

---
+++ main/gatt.h
@@ -0,0 +1,60 @@
+#ifndef GATT_H
+#define GATT_H
+
+// C includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// FreeRTOS includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+// ESP system includes
+#include "esp_system.h"
+// Logging includes
+#include "esp_log.h"
+// NVS includes
+#include "nvs_flash.h"
+// Bluetooth includes
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "tag.h"
+
+#define REMOTE_SERVICE_UUID        0x00FF
+#define REMOTE_NOTIFY_CHAR_UUID    0xFF01
+#define PROFILE_NUM      1
+#define PROFILE_A_APP_ID 0
+#define INVALID_HANDLE   0
+
+typedef struct
+{
+    esp_gattc_cb_t gattc_cb;
+    uint16_t gattc_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_start_handle;
+    uint16_t service_end_handle;
+    uint16_t char_handle;
+    esp_bd_addr_t remote_bda;
+} gattc_profile_inst;
+
+extern gattc_profile_inst profile_tab[PROFILE_NUM];
+
+void esp_gap_cb(
+	esp_gap_ble_cb_event_t event,
+	esp_ble_gap_cb_param_t *param);
+void esp_gattc_cb(
+	esp_gattc_cb_event_t event,
+	esp_gatt_if_t gattc_if,
+	esp_ble_gattc_cb_param_t *param);
+void gattc_profile_event_handler(
+	esp_gattc_cb_event_t event,
+	esp_gatt_if_t gattc_if,
+	esp_ble_gattc_cb_param_t *param);
+
+#endif
